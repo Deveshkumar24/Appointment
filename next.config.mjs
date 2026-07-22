7475537d9cece0f54ab411f16deba8db5a17986e
Devesh Kumar <deveshkyash@gmail.com>
2026-07-22 20:48:24 +0530

    ci: adapt configurations for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/Appointment',
+  /* config options here */
 };
 
 export default nextConfig;
